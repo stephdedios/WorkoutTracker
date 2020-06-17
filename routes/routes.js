2783ea4e0a4f3fa909f533fd2ac49e41ec53b005
Stephanie DeDios <stephtdedios@gmail.com>
2020-06-17 09:02:54 -0700

    updated routes

---
+++ routes/routes.js
@@ -0,0 +1,72 @@
+const router = require("express").Router();
+const path = require("path");
+var db = require("../models/workout");
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .sort({ day: 1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+      console.log(1);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+      console.log(3);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts/", (req, res) => {
+  console.log("createWorkout", req.body);
+  db.Workout.create({
+    exercises: [req.body],
+  })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+      console.log(5, dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+      console.log(7);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(require("./routes/routes"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nosql", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
